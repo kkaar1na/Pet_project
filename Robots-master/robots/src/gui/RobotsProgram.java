afdb934e63b4f9d65b03ba4a69389b4fd22b72f1
karina <karinavazetdinova2406@gmail.com>
2026-02-21 10:35:38 +0500

    исправление решения дополнительного задания

--- Robots-master/robots/src/gui/RobotsProgram.java
+++ Robots-master/robots/src/gui/RobotsProgram.java
@@ -9,38 +9,27 @@ import javax.swing.UIManager;
 
 public class RobotsProgram
 {
-  public static void main(String[] args) {
-    try {
-      Locale.setDefault(new Locale("ru", "RU"));
+    public static void main(String[] args) {
+        try {
+            Locale.setDefault(new Locale("ru", "RU"));
 
-      try {
-        ResourceBundle bundle = ResourceBundle.getBundle("swing_ru");
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
 
-        UIManager.put("OptionPane.yesButtonText", bundle.getString("OptionPane.yesButtonText"));
-        UIManager.put("OptionPane.noButtonText", bundle.getString("OptionPane.noButtonText"));
-        UIManager.put("OptionPane.cancelButtonText", bundle.getString("OptionPane.cancelButtonText"));
-        UIManager.put("OptionPane.okButtonText", bundle.getString("OptionPane.okButtonText"));
-        UIManager.put("FileChooser.openButtonText", bundle.getString("FileChooser.openButtonText"));
-        UIManager.put("FileChooser.saveButtonText", bundle.getString("FileChooser.saveButtonText"));
-        UIManager.put("FileChooser.cancelButtonText", bundle.getString("FileChooser.cancelButtonText"));
+            ResourceBundle bundle = ResourceBundle.getBundle("swing_ru");
 
-      } catch (Exception e) {
-        System.out.println("Ресурсы русские не удалось загрузить: " + e.getMessage());
-      }
+            for (String key : bundle.keySet()) {
+                UIManager.put(key, bundle.getString(key));
+            }
 
-      UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-//        UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-//        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-//        UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-    } catch (Exception e) {
-      e.printStackTrace();
+        SwingUtilities.invokeLater(() -> {
+            MainApplicationFrame frame = new MainApplicationFrame();
+            frame.pack();
+            frame.setVisible(true);
+            frame.setExtendedState(Frame.MAXIMIZED_BOTH);
+        });
     }
-    SwingUtilities.invokeLater(() -> {
-      MainApplicationFrame frame = new MainApplicationFrame();
-      frame.pack();
-      frame.setVisible(true);
-      frame.setExtendedState(Frame.MAXIMIZED_BOTH);
-    });
-  }
 }
